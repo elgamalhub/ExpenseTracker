0c35ea49aa0b8659cbd248a4af40512134d4d416
Ahmed Abdelrahman Elgamal <48561510+elgamalhub@users.noreply.github.com>
2026-09-21 01:53:19 +0300

    Update cache name and fix icon paths in sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = "ledger-cache-v1";
+const CACHE_NAME = "ledger-cache-v2";
 
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/icon-512-maskable.png"
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-512-maskable.png"
 ];
 
 self.addEventListener("install", (event) => {
